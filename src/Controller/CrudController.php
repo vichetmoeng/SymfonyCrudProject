84bfd496f3305c4f5ba40f502bf97384a762ffbe
Vichet <54062393+vichetmoeng@users.noreply.github.com>
2020-11-01 11:12:04 +0700

    added crud method

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -15,4 +15,28 @@ class CrudController extends AbstractController
     {
         return $this->render('crud/index.html.twig');
     }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create()
+    {
+        exit('to do somthing ');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus($id)
+    {
+        exit('Todo somethign '. $id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete_task")
+     */
+    public function delete($id)
+    {
+        exit('Todo somethign '. $id);
+    }
 }
